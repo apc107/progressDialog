30ed9fd9b17e203efb2c8be82af34224f1f1eb21
Michael Y. C. Shih <i420cc@gmail.com>
2018-06-29 08:02:42 +0800

    initial

---
+++ app/src/main/java/com/example/mycs/progressdialog/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.mycs.progressdialog;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMax(100);
+                progressDialog.setMessage("It's loading...");
+                progressDialog.setTitle("Progress Dialog with bar example");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            while (progressDialog.getProgress() <= progressDialog.getMax()) {
+                                Thread.sleep(200);
+                                handle.sendMessage(handle.obtainMessage());
+                                if(progressDialog.getProgress() == progressDialog.getMax()) {
+                                    progressDialog.dismiss();
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+            @SuppressLint("HandlerLeak")
+            Handler handle = new Handler() {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+                    progressDialog.incrementProgressBy(1);
+                }
+            };
+        });
+    }
+}
